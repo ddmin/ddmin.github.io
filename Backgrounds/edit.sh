b77c62d136f7027663cf8db0d1b11db87f706b65
ddmin <ddmin216@outlook.com>
2021-08-14 01:00:10 -0400

    🪟 blur images

---
+++ Backgrounds/edit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Blur and Darken wallpapers
+
+ORIGINAL="./original"
+SECONDS=0
+FILES=$(ls -l "$ORIGINAL" | wc -l)
+let COUNT=FILES-2
+
+for i in $( eval echo {0..$COUNT} )
+do
+    echo "Modifying $i.jpg"
+    convert -brightness-contrast -8x5 -blur 6x8 "$ORIGINAL/$i.jpg" "$i.jpg"
+done
+
+msg="Finished in $((($SECONDS)/60))m $((($SECONDS)%60))s"
+len=$((17+${#d}))
+printf '=%.0s' `eval echo {1..$len}`
+echo ""
+echo $msg
+printf '=%.0s' `eval echo {1..$len}`
+echo ""
